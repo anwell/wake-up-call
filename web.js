e2572d28e6db791260d6509d87d22bd592cddb99
Anwell Wang <anwell@rainarcade.com>
2013-01-01 02:25:13 -0800

    hard coded concatenation of strings

--- web.js
+++ web.js
@@ -53,14 +53,9 @@ app.all('/', function(request, response) {
         var num1 = Math.floor(Math.random()*8 +3);
         var num2 = Math.floor(Math.random()*39 +11);
         var answer = num1 * num2;
-        answer = parseInt(answer);                                      
-        var titles = "<Response>";
+        answer = parseInt(answer);
         //using the gather TwiML to receive keypad input
-        titles += "<Gather timeout='30' finishOnKey='*'>";
-        titles += ("<Say>" + "Good Morning. What is " + num1 + " times " + num2 + "</Say>");
-        titles += "</Gather>";
-        titles += "</Response>";
-        response.send(titles);
+        response.send("<Response><Gather timeout='30' finishOnKey='*'><Say>Good Morning. What is " + num1 + " times " + num2 + "</Say></Gather></Response>";);
 
         answers[request.body.From] = answer;
         //checking if the answer was answered correctly, meaning the answer would be 0
@@ -74,24 +69,15 @@ app.all('/', function(request, response) {
             var input = request.body.Digits;
             input = parseInt(input);
             if (input === parseInt(answers[request.body.From], 10)) {
-                var right = "<Response>";
-                right += "<Say>" + "Good morning." + "</Say>";
-                right += "</Response>";
-                response.send(right);
+                response.send("<Response><Say>That is correct. Good morning and goodbye.</Say></Response>");
                 answers[request.body.From] = 0; // 0 means the answer was inputted correctly
             } else {
-                var wrong = "<Response>";
-                wrong += "<Say>" + "I'm sorry, that's wrong. This is what you entered." + input + "</Say>";
-                wrong += "</Response>";
+                var wrong = "<Response><Say>I'm sorry, that's wrong. This is what you entered." + input + "</Say></Response>";
                 response.send(wrong);
-                console.log(input);
             }
         }
  
         else {
-            var = "<Response>";
-            dne += "<Say>" + "We didn't receive any input. Goodbye!" + "</Say>";
-            dne += "</Response>";
             response.send("<Response><Say>We didn't receive any input. Goodbye!</Say></Response>");
         }
     }
@@ -117,11 +103,11 @@ app.all('/', function(request, response) {
         console.log("successful cron job creation")
     } catch(ex) {
         console.log("cron job incorrectly formatted")
-        //say how to format the text message
-
+        //#TODOsay how to format the text message
     }
 
-    //response.send(200); //Right now not all requests are met with responses
+    //response.send(200); //Send a reply saying OK
+    //Because there is no response, heroku logs an error
 });
 
 //heroku assigns ports automatically
